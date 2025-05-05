32abb168b975df7186663e4de46c158f51875b51
Matvii Mashtaliar <matvii.mashtalyar@gmail.com>
2025-05-05 22:36:42 +0300

    Add validation for token programs

--- src/solana-service/program.ts
+++ src/solana-service/program.ts
@@ -1,19 +1,20 @@
-import { AnchorProvider, Program, Wallet, web3, BN } from "@coral-xyz/anchor";
-import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
-import { PublicKey } from "@solana/web3.js";
+import { AnchorProvider, Program, Wallet, web3, BN } from '@coral-xyz/anchor';
+import { PublicKey } from '@solana/web3.js';
 
-import escrowIdl from "./escrow.json";
-import { Escrow } from "./idlType";
-import { config } from "./config";
+import escrowIdl from './escrow.json';
+import { Escrow } from './idlType';
+import { config } from './config';
+import { getAssociatedTokenAddressSync } from '@solana/spl-token';
+import { isTokenProgram } from '@/utils';
 
 export class EscrowProgram {
   protected program: Program<Escrow>;
   protected connection: web3.Connection;
-  protected wallet: NodeWallet;
+  protected wallet: Wallet;
 
   constructor(connection: web3.Connection, wallet: Wallet) {
     const provider = new AnchorProvider(connection, wallet, {
-      commitment: "confirmed",
+      commitment: 'confirmed',
     });
     this.program = new Program<Escrow>(escrowIdl as Escrow, provider);
     this.wallet = wallet;
@@ -23,9 +24,9 @@ export class EscrowProgram {
   createOfferId = (offerId: BN) => {
     return PublicKey.findProgramAddressSync(
       [
-        Buffer.from("offer"),
+        Buffer.from('offer'),
         this.wallet.publicKey.toBuffer(),
-        offerId.toArrayLike(Buffer, "le", 8),
+        offerId.toArrayLike(Buffer, 'le', 8),
       ],
       new PublicKey(config.contractAddress)
     )[0];
@@ -38,8 +39,56 @@ export class EscrowProgram {
     tokenAmountB: number
   ) {
     try {
-      console.log(tokenMintA, tokenMintB, tokenAmountA, tokenAmountB);
-      return null;
+      const [mintAInfo, mintBInfo] = await Promise.all([
+        this.connection.getAccountInfo(tokenMintA),
+        this.connection.getAccountInfo(tokenMintB),
+      ]);
+
+      if (!mintAInfo || !mintBInfo) {
+        throw new Error('Mint not found');
+      }
+
+      const tokenProgramMintA = mintAInfo.owner;
+      const tokenProgramMintB = mintBInfo.owner;
+
+      if (tokenProgramMintA.toBase58() !== tokenProgramMintB.toBase58()) {
+        throw new Error('Token programs do not match');
+      }
+
+      const tokenProgram = tokenProgramMintA;
+
+      if (!isTokenProgram(tokenProgram)) {
+        throw new Error('Program is not a token program');
+      }
+      
+      const randomOfferId = new BN(Math.floor(Math.random() * 1000000000000));
+
+      const makerTokenAccountA = getAssociatedTokenAddressSync(
+        tokenMintA,
+        this.wallet.publicKey,
+        false,
+        tokenProgram
+      );
+
+      const offerAddress = this.createOfferId(randomOfferId);
+
+      const vault = getAssociatedTokenAddressSync(
+        tokenMintA,
+        offerAddress,
+        true,
+        tokenProgram
+      );
+
+      return this.program.methods
+        .makeOffer(randomOfferId, tokenAmountA, tokenAmountB)
+        .accounts({
+          makerTokenAccountA,
+          tokenMintA,
+          tokenMintB,
+          tokenProgram,
+          vault,
+        })
+        .rpc();
     } catch (e) {
       console.log(e);
       return null;
@@ -53,8 +102,45 @@ export class EscrowProgram {
     tokenMintB: PublicKey
   ) {
     try {
-      console.log(maker, offer, tokenMintA, tokenMintB);
-      return null;
+      const mintAInfo = await this.connection.getAccountInfo(tokenMintA);
+      if (!mintAInfo) {
+        throw new Error('Mint A not found');
+      }
+      const tokenProgramMint = mintAInfo.owner; // TODO: handle different token programs
+      const makerTokenAccountB = getAssociatedTokenAddressSync(
+        tokenMintB,
+        maker,
+        false,
+        tokenProgramMint
+      );
+      const takerTokenAccountA = getAssociatedTokenAddressSync(
+        tokenMintA,
+        this.wallet.publicKey,
+        false,
+        tokenProgramMint
+      );
+      const takerTokenAccountB = getAssociatedTokenAddressSync(
+        tokenMintB,
+        this.wallet.publicKey,
+        false,
+        tokenProgramMint
+      );
+      const vault = getAssociatedTokenAddressSync(
+        tokenMintA,
+        offer,
+        true,
+        tokenProgramMint
+      );
+      return this.program.methods
+        .takeOffer()
+        .accounts({
+          vault,
+          tokenProgram: tokenProgramMint,
+          makerTokenAccountB,
+          takerTokenAccountA,
+          takerTokenAccountB,
+        })
+        .rpc();
     } catch (e) {
       console.log(e);
       return null;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,26 +1,28 @@
-import { clsx, type ClassValue } from "clsx";
-import { gql } from "graphql-request";
-import { twMerge } from "tailwind-merge";
+import { TOKEN_2022_PROGRAM_ID, TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { PublicKey } from '@solana/web3.js';
+import { clsx, type ClassValue } from 'clsx';
+import { gql } from 'graphql-request';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export function truncateAddress(address: string, length: number = 8): string {
-  if (!address) return "";
+  if (!address) return '';
   return `${address.slice(0, length)}...${address.slice(-length)}`;
 }
 
 export const METADATA = {
-  ["GdHsojisNu8RH92k4JzF1ULzutZgfg8WRL5cHkoW2HCK"]: {
-    address: "GdHsojisNu8RH92k4JzF1ULzutZgfg8WRL5cHkoW2HCK",
-    icon: "🌭",
-    symbol: "HOT",
+  ['GdHsojisNu8RH92k4JzF1ULzutZgfg8WRL5cHkoW2HCK']: {
+    address: 'GdHsojisNu8RH92k4JzF1ULzutZgfg8WRL5cHkoW2HCK',
+    icon: '🌭',
+    symbol: 'HOT',
   },
-  ["9NCKufE7BQrTXTang2WjXjBe2vdrfKArRMq2Nwmn4o8S"]: {
-    address: "9NCKufE7BQrTXTang2WjXjBe2vdrfKArRMq2Nwmn4o8S",
-    icon: "🍔",
-    symbol: "Burger",
+  ['9NCKufE7BQrTXTang2WjXjBe2vdrfKArRMq2Nwmn4o8S']: {
+    address: '9NCKufE7BQrTXTang2WjXjBe2vdrfKArRMq2Nwmn4o8S',
+    icon: '🍔',
+    symbol: 'Burger',
   },
 };
 
@@ -83,6 +85,10 @@ export const getUserOffers = (acctMaker: string) => gql`
 `;
 
 export const createPass = (walletAddress?: string | null) => {
-  if (!walletAddress) return "Connect your wallet";
-  return walletAddress.slice(0, 3) + "NU" + walletAddress.slice(-8) + "LL";
+  if (!walletAddress) return 'Connect your wallet';
+  return walletAddress.slice(0, 3) + 'NU' + walletAddress.slice(-8) + 'LL';
 };
+
+export function isTokenProgram(address: PublicKey) {
+  return [TOKEN_PROGRAM_ID, TOKEN_2022_PROGRAM_ID].includes(address);
+}
